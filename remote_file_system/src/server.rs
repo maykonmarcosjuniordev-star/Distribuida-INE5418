a4951d158f66a1e81612fc23f35ee5bcf15e0c33
Gabriel-Turatti <75402522+Gabriel-Turatti@users.noreply.github.com>
2025-09-24 14:36:29 -0300

    Operações principais do server (not tested)
    abre, le, escreve, fecha.
    Adiciona hashmap de acesso dos clientes pros arquivos

--- remote_file_system/src/server.rs
+++ remote_file_system/src/server.rs
@@ -8,14 +8,23 @@ pub struct Server {
     address: SocketAddr,
     socket: TcpStream,
     socket_udp: UdpSocket,
-    // clientes: Vec<>,
-    // buffer: Vec<>
+    current_client: IpAddr,
+    users: HashMap<u32, Vec<IpAddr>>, // Map of file descriptors, in each position are the clients using it.
+    buffer: Vec<u8>,
 }
 
 pub struct Request {
     request_type: u8,
-    file_op: u8,
-    file_name: String,
+    file_read_or_write: u8,
+    file_id: u8,
+    posicao: u32,
+    size: u32,
+    data: String,
+}
+
+pub struct Response {
+    response_type: u8, // 1 -> Atualiza cache dos clientes devido a write
+    data: String,
 }
 
 impl Server {
@@ -32,30 +41,59 @@ impl Server {
         self.address
     }
 
-    pub fn abre(&self, descritor_arquivo: i32, nome_arquivo: String) -> i32 {
-        self.files.abre(descritor_arquivo, nome_arquivo)
+    pub fn abre(&self, nome_arquivo: String, op: u8) -> i32 {
+        let mut descritor_arquivo;
+        match self.files.abre(descritor_arquivo, nome_arquivo) {
+            0 => { // TODO diferença entre write e read
+                if op == 1 {
+                    users[descritor_arquivo].push(current_client); // write...?
+                } else {
+                    users[descritor_arquivo].push(current_client); // read...?
+                }
+                0
+            }
+            Some(i) => i
+        }
     }
 
-    // pub fn le(&self, descritor_arquivo: i32, posicao: u64, buffer: &mut Vec<u8>, tamanho: usize) {
-    //     match self.files.le(descritor_arquivo, posicao: u64, buffer: &mut Vec<u8>, tamanho: usize) {
-    //         Some(t) => 0,
-    //         Err(e) => e,
-    //     }
-    // }
+    pub fn le(&self, descritor_arquivo: i32, posicao: u64, tamanho: usize) {
+        self.files.le(descritor_arquivo, posicao, self.buffer, tamanho);
+        self.send(self.buffer, self.src);
+    }
 
-    // pub fn enviar_buffer(buffer: Vec<u8>, dest: IpAddr) {
+    pub fn escreve(&self, descritor_arquivo: u32, posicao: u64, buffer: &mut Vec<u8>, tamanho: usize) {
+        self.files.escreve(descritor_arquivo, posicao, buffer, tamanho);
+        
+        self.send(buffer, self.src); // Precisa avisar o cliente que a escrita deu certo?
+        // TODO Atualizar todos os clientes que estão com esse arquivo aberto, que ele foi alterado.
+        Response {response_type: 1}
+    }
 
-    //     socket.send_to(buf, &dest)?;
-    // }
+    pub fn fecha(&self, descritor_arquivo: u32) {
+        self.files.fecha(descritor_arquivo)
+
+        users[descritor_arquivo].retain(|&x| x != self.src); // remove(?) cliente da lista
+    }
+
+    pub fn send(buffer: Vec<u8>, src: IpAddr) {
+        socket.send_to(&buffer, src);
+    }
 
     pub fn desserialize(buffer: Vec<u8>) -> Request {
-        Request {request_type: buffer[0], file_op: buffer[1], file_name: String::from_utf8(buffer[2..]).unwrap()}
+        Request {
+            request_type: buffer[0],
+            file_read_or_write: buffer[1],
+            file_id: buffer[2],
+            posicao: u32::from_be_bytes(buffer[3..6].try_into().unwrap()),
+            size: u32::from_be_bytes(buffer[7..10].try_into().unwrap()),
+            data: String::from_utf8(buffer[11..]).unwrap()
+        }
     }
 
     pub fn main(self) {
         let mut bufferSocket = [0; 1024];
-        // self.vetor de clientes, add src
         while (let Ok((amt, src)) = self.socket.recv_from(&mut bufferSocket)) {
+            self.current_client = src;
             let mut bufferVect = vec![];
             let requisito = self.desserialize(bufferSocket);
 
@@ -66,15 +104,12 @@ impl Server {
             let requisito = self.desserialize(buffer);
 
             match requisito.request_type {
-                0 => self.abre(),
-                1 => self.le(),
-                2 => self.escreve(),
-                3 => self.fecha(),
+                0 => self.abre(requisito.data, requisito.file_read_or_write),
+                1 => self.le(requisito.file_id, requisito.posicao, requisito.size),
+                2 => self.escreve(requisito.file_id, requisito.posicao, requisito.data, requisito.size),
+                3 => self.fecha(requisito.file_id),
                 _ => {},
             };
-
-
-            // cliente.enviar(buffer);
         }
     }
 }
