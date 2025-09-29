635b17e72dd0304b67f47ed69d3bfa181583dd4c
maykonmarcosjuniordev-star <maykon.marcos.junior.dev@gmail.com>
2025-09-29 19:55:21 -0300

    Fixing up Socket use

--- remote_file_system/src/file_manager.rs
+++ remote_file_system/src/file_manager.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 use std::os::unix::fs::FileExt;
 
 pub struct FileManager {
-    file_table: HashMap<u32, String>,
+    file_table: HashMap<u32, File>,
 }
 
 impl FileManager {
@@ -18,13 +18,12 @@ impl FileManager {
     /// 
     /// ● o valor de retorno inteiro (int) deve representar códigos de erro, na
     /// impossibilidade de execução da operação;
-    pub fn abre(&self, descritor_arquivo: u32, nome_arquivo: String) -> i32 {
+    pub fn abre(&mut self, descritor_arquivo: u32, nome_arquivo: String) -> i32 {
         // verify if it is already on the table
         if self.file_table.contains_key(&descritor_arquivo) {
             return 0;
         }
-        // TODO: usar lifetime para permitir que o arquivo saia de escopo
-        let _file: File = match OpenOptions::new()
+        let file: File = match OpenOptions::new()
         .create(true)
         .open(&nome_arquivo) {
             Ok(f) => f,
@@ -33,6 +32,7 @@ impl FileManager {
                 return -1;
             },
         };
+        self.file_table.insert(descritor_arquivo, file);
         0
     }
     /// ● descritor_arquivo indica o identificador do descritor ao qual se pretende manipular;
@@ -48,25 +48,13 @@ impl FileManager {
     ///     na impossibilidade de execução da operação.
     pub fn le(&self, descritor_arquivo: u32, posicao: u64, buffer: &mut Vec<u8>, tamanho: usize) -> i32 {
         match self.file_table.get(&descritor_arquivo) {
-            Some(nome_arquivo) => {
-                let file = match OpenOptions::new().read(true).open(&nome_arquivo) {
-                    Ok(f) => {f}
-                    Err(e) => {
-                        println!("Erro {} ao abrir arquivo: {}", e, nome_arquivo);
-                        return -1;
-                    }
-                };
+            Some(file) => {
                 match  file.read_exact_at(buffer[..tamanho].as_mut(), posicao) {
                     Ok(_) => {
                         buffer.len() as i32
-                        // if buffer.len() == tamanho {
-                        //     tamanho as i32
-                        // } else {
-                        //     buffer.len() as i32
-                        // }
                     }
                     Err(e) => {
-                        println!("Erro {} ao abrir arquivo: {}", e, nome_arquivo);
+                        println!("Erro {} ao abrir arquivo: {}", e, descritor_arquivo);
                         -1
                     }
                 }
@@ -92,25 +80,13 @@ impl FileManager {
     ///     na impossibilidade de execução da operação.
     pub fn escreve(&self, descritor_arquivo: u32, posicao: u64, buffer: &mut Vec<u8>, tamanho: usize) -> i32 {
         match self.file_table.get(&descritor_arquivo) {
-            Some(nome_arquivo) => {
-                let file = match OpenOptions::new().read(true).open(&nome_arquivo) {
-                    Ok(f) => {f}
-                    Err(e) => {
-                        println!("Erro {} ao abrir arquivo: {}", e, nome_arquivo);
-                        return -1;
-                    }                    
-                };
+            Some(file) => {
                 match  file.write_all_at(buffer[..tamanho].as_mut(), posicao) {
                     Ok(_) => {
-                        buffer.len()
-                        // if buffer.len() == tamanho {
-                        //     tamanho as i32
-                        // } else {
-                        //     buffer.len() as i32
-                        // }
+                        buffer.len() as i32
                     }
                     Err(e) => {
-                        println!("Erro {} ao abrir arquivo: {}", e, nome_arquivo);
+                        println!("Erro {} ao abrir arquivo: {}", e, descritor_arquivo);
                         -1
                     }
                 }
@@ -125,16 +101,8 @@ impl FileManager {
         if !self.file_table.contains_key(&descritor_arquivo) {
             return -1;
         }
-        let nome_arquivo = self.file_table.get(&descritor_arquivo).expect("shouldn't happen");
+        let file = self.file_table.get(&descritor_arquivo).expect("shouldn't happen");
         // fecha o arquivo
-        let file = match OpenOptions::new()
-        .open(&nome_arquivo) {
-            Ok(f) => f,
-            Err(e) => {
-                println!("Erro {} ao fechar arquivo: {}", e, nome_arquivo);
-                return -1;
-            },
-        };
         drop(file);
         0
     }
--- remote_file_system/src/server.rs
+++ remote_file_system/src/server.rs
@@ -1,5 +1,6 @@
-use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpStream, UdpSocket};
-// use std::sync::mpsc::{Sender, self};
+use std::io::{Write, Read};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpStream, TcpListener, UdpSocket};
+use std::collections::HashMap;
 
 use crate::file_manager::FileManager;
 
@@ -8,16 +9,14 @@ pub struct Server {
     address: SocketAddr,
     socket: TcpStream,
     socket_udp: UdpSocket,
-    current_client: IpAddr,
-    users: HashMap<u32, Vec<IpAddr>>, // Map of file descriptors, in each position are the clients using it.
-    buffer: Vec<u8>,
+    // Map of file descriptors, in each position are the clients using it.
+    users: HashMap<u32, Vec<SocketAddr>>, 
 }
 
 pub struct Request {
     request_type: u8,
-    file_read_or_write: u8,
-    file_id: u8,
-    posicao: u32,
+    descritor_arquivo: u32,
+    posicao: u64,
     size: u32,
     data: String,
 }
@@ -34,82 +33,127 @@ impl Server {
         let address = SocketAddr::new(IpAddr::V4(ip), port);
         let socket = TcpStream::connect(address).unwrap();
         let socket_udp = UdpSocket::bind(address).unwrap();
-        Self {files, address, socket, socket_udp}
+        let users = HashMap::new();
+        Self {files, address, socket, socket_udp, users}
     }
 
     pub fn get_address(&self) -> SocketAddr {
         self.address
     }
 
-    pub fn abre(&self, nome_arquivo: String, op: u8) -> i32 {
-        let mut descritor_arquivo;
+    /// Chama o file manager para abrir o arquivo
+    pub fn abre(&mut self, descritor_arquivo: u32,
+                nome_arquivo: String, client: SocketAddr) -> i32 {
         match self.files.abre(descritor_arquivo, nome_arquivo) {
-            0 => { // TODO diferença entre write e read
-                if op == 1 {
-                    users[descritor_arquivo].push(current_client); // write...?
-                } else {
-                    users[descritor_arquivo].push(current_client); // read...?
-                }
+            0 => {
+                let usr = self.users
+                    .get_mut(&descritor_arquivo)
+                    .expect("shouldn't happen");
+                usr.push(client);
                 0
             }
-            Some(i) => i
+            i => {i}
         }
     }
 
-    pub fn le(&self, descritor_arquivo: i32, posicao: u64, tamanho: usize) {
-        self.files.le(descritor_arquivo, posicao, self.buffer, tamanho);
-        self.send(self.buffer, self.src);
+    /// Chama o file manager para ler o arquivo e envia o buffer para o cliente
+    pub fn le(&self, descritor_arquivo: u32, posicao: u64,
+                tamanho: usize, client: SocketAddr) {
+        let mut buffer: Vec<u8> = vec![0; tamanho];
+        // TODO: Deveria verificar se o cliente tem o arquivo aberto
+        // TODO: Deveria verificar se o arquivo não está sendo escrito por outro cliente
+        self.files.le(descritor_arquivo, posicao, &mut buffer, tamanho);
+        self.send(buffer, client);
     }
 
-    pub fn escreve(&self, descritor_arquivo: u32, posicao: u64, buffer: &mut Vec<u8>, tamanho: usize) {
-        self.files.escreve(descritor_arquivo, posicao, buffer, tamanho);
-        
-        self.send(buffer, self.src); // Precisa avisar o cliente que a escrita deu certo?
-        // TODO Atualizar todos os clientes que estão com esse arquivo aberto, que ele foi alterado.
-        Response {response_type: 1}
+    /// Chama o file manager para escrever no arquivo.
+    /// Invalida os caches dos outros clientes que possuem o arquivo aberto
+    pub fn escreve(&self, descritor_arquivo: u32, posicao: u64,
+                buffer: &mut Vec<u8>, tamanho: usize, client: SocketAddr) -> i32 {
+        let out = self.files.escreve(descritor_arquivo, posicao, buffer, tamanho);
+        // TODO: Invalida os caches dos outros clientes que possuem o arquivo aberto
+        let usrs = self.users
+            .get(&descritor_arquivo)
+            .expect("shouldn't happen");
+        return out;
     }
 
-    pub fn fecha(&self, descritor_arquivo: u32) {
-        self.files.fecha(descritor_arquivo)
+    /// Chama o file manager para fechar o arquivo
+    /// Remove o cliente da lista de usuários do arquivo
+    pub fn fecha(&mut self, descritor_arquivo: u32, client: SocketAddr) -> i32 {
+        let out = self.files.fecha(descritor_arquivo);
 
-        users[descritor_arquivo].retain(|&x| x != self.src); // remove(?) cliente da lista
+        // TODO: Não sei se isso funciona
+        self.users.get_mut(&descritor_arquivo).expect("File not found")
+                .retain(|&x| x != client); // remove(?) cliente da lista
+        return out;
     }
 
-    pub fn send(buffer: Vec<u8>, src: IpAddr) {
-        socket.send_to(&buffer, src);
+    /// Envia o buffer para o endereço do cliente
+    /// Utiliza o Socket TCP
+    pub fn send(&self, buffer: Vec<u8>, src: SocketAddr) {
+        let mut stream = TcpStream::connect(src).unwrap();
+        stream.write(&buffer).unwrap();
     }
 
-    pub fn desserialize(buffer: Vec<u8>) -> Request {
+    /// Desserializa o buffer recebido do cliente
+    pub fn desserialize(buffer: &Vec<u8>) -> Request {
+        // Expected layout:
+        // [0]                 -> request_type (1 byte)
+        // [1..5]              -> descritor_arquivo (4 bytes, big-endian)
+        // [5..9]              -> posicao (4 bytes, big-endian)
+        // [9..13]             -> size (4 bytes, big-endian)
+        // [13..]              -> data (remaining bytes)
         Request {
             request_type: buffer[0],
-            file_read_or_write: buffer[1],
-            file_id: buffer[2],
-            posicao: u32::from_be_bytes(buffer[3..6].try_into().unwrap()),
-            size: u32::from_be_bytes(buffer[7..10].try_into().unwrap()),
-            data: String::from_utf8(buffer[11..]).unwrap()
+            descritor_arquivo: u32::from_be_bytes(buffer[1..5].try_into().unwrap()),
+            posicao: u64::from_be_bytes(buffer[5..13].try_into().unwrap()),
+            size: u32::from_be_bytes(buffer[13..17].try_into().unwrap()),
+            data: String::from_utf8(buffer[17..].to_vec()).unwrap(),
         }
     }
 
-    pub fn main(self) {
-        let mut bufferSocket = [0; 1024];
-        while (let Ok((amt, src)) = self.socket.recv_from(&mut bufferSocket)) {
-            self.current_client = src;
-            let mut bufferVect = vec![];
-            let requisito = self.desserialize(bufferSocket);
-
-            for i in 0..amt {
-                bufferVect.push(bufferSocket[i]);
+    pub fn main(&mut self) {        
+        // reads from socket
+        let listener = TcpListener::bind(self.address).unwrap();
+        for stream in listener.incoming() {
+            match stream {
+                Ok(mut stream) => {
+                    let current_client = stream.peer_addr().unwrap();
+                    println!("New connection: {}", current_client);
+                    let mut buffer_socket = vec![0; 1024];
+                    let amt = stream.read(&mut buffer_socket).unwrap();
+                    println!("Received {} bytes from {}", amt, current_client);
+                    let mut bufferVect = vec![];
+                    let requisito = Self::desserialize(&buffer_socket);
+        
+                    for i in 0..amt {
+                        bufferVect.push(buffer_socket[i].clone());
+                    }
+        
+                    match requisito.request_type {
+                        0 => {
+                            self.abre(requisito.descritor_arquivo, requisito.data, current_client);
+                        },
+                        1 => {
+                            self.le(requisito.descritor_arquivo, requisito.posicao, requisito.size as usize, current_client);
+                        },
+                        2 => {
+                            let mut buffer = requisito.data.as_bytes().to_vec();
+                            self.escreve(requisito.descritor_arquivo, requisito.posicao, &mut buffer, requisito.size as usize, current_client);
+                        },
+                        3 => {
+                            self.fecha(requisito.descritor_arquivo, current_client);
+                        },
+                        _ => {
+                            println!("Invalid request type");
+                        },
+                    };
+                }
+                Err(e) => {
+                    println!("Error: {}", e);
+                }
             }
-
-            let requisito = self.desserialize(buffer);
-
-            match requisito.request_type {
-                0 => self.abre(requisito.data, requisito.file_read_or_write),
-                1 => self.le(requisito.file_id, requisito.posicao, requisito.size),
-                2 => self.escreve(requisito.file_id, requisito.posicao, requisito.data, requisito.size),
-                3 => self.fecha(requisito.file_id),
-                _ => {},
-            };
         }
     }
 }
